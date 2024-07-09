cc3b9e2da119b384409f829aa223fba240aa9255
write2shivamgithub <shivamshankaraacharya@gmail.com>
2024-07-09 13:16:12 +0530

    testing

--- src/model_training.py
+++ src/model_training.py
@@ -18,8 +18,8 @@ y = df['Placed']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state = 42)
 
-n_estimators = 100
-max_depth = 10
+n_estimators = 50
+max_depth = 20
 
 rf = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth)
 
